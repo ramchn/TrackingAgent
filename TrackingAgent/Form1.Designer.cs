4e51a4f23e54ee450d2944e120195df71596ce1f
ramchn <ramamoorthyp@gmail.com>
2021-10-20 20:12:09 +0530

    initial draft

---
+++ TrackingAgent/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace TrackingAgent
+{
+    partial class TrackingAgent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {            
+            this.button = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(22, 23);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 1;
+            this.button.Text = "Start";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(123, 26);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 17);
+            this.status.TabIndex = 3;
+            // 
+            // TrackingAgent
+            // 
+            this.ClientSize = new System.Drawing.Size(323, 81);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.button);
+            this.Name = "TrackingAgent";
+            this.Text = "Tracking Agent";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label status;
+    }
+}
+
---
+++ TrackingAgent/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TrackingAgent
+{
+    public partial class TrackingAgent : Form
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hwnd, StringBuilder ss, int count);
+
+        Timer tmr = new Timer(); // capture the tracking info
+        Timer tmr2 = new Timer(); // send the captured info to server
+        String filepath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/log.txt";
+        readonly HttpClient client = new HttpClient();
+
+        public TrackingAgent()
+        {
+            InitializeComponent();
+            this.TopMost = true;
+
+            if (!File.Exists(filepath))
+            {
+                File.Create(filepath);
+            }
+
+            tmr.Interval = 1000 * 30; // 30secs
+            tmr.Tick += Tmr_Tick;
+
+            tmr2.Interval = 1000 * 60 * 15; // 15mins
+            tmr2.Tick += Tmr_Tick2;            
+        }
+
+        private void Tmr_Tick(object sender, EventArgs e)
+        {
+            //get title of active window
+            string title = ActiveWindowTitle();
+            //check if it is null and add it to list if correct
+            if (title != "")
+            {
+                TextWriter writeFile = new StreamWriter(filepath, true);
+                //write data into file
+                writeFile.WriteLine("{\"timestamp\":\""+DateTime.Now.ToString("hh:mm:ss")+"\",\"title\":\""+title+"\"}");
+                writeFile.Flush();
+                writeFile.Close();
+            }
+        }
+
+        private void Tmr_Tick2(object sender, EventArgs e)
+        {            
+            tmr.Stop();
+            StringBuilder content = new StringBuilder();
+            string str = String.Empty;
+            TextReader readFile = new StreamReader(filepath);
+            while ((str = readFile.ReadLine()) != null)
+            {
+                content.Append(str + ",");
+            }
+            content.Length--;
+            readFile.Close();
+            File.WriteAllText(filepath, "");
+            tmr.Start();
+            Console.WriteLine("file content: " + content);
+            var strcontent = new StringContent("[" + content + "]", Encoding.UTF8, "application/json");
+            var response = client.PostAsync("http://localhost:3000/create", strcontent);
+            string result = ((StreamContent)response.Result.Content).ReadAsStringAsync().Result;
+            Console.WriteLine("http result: " + result);            
+        }
+
+        private string ActiveWindowTitle()
+        {
+            //Create the variable
+            const int nChar = 256;
+            StringBuilder ss = new StringBuilder(nChar);
+
+            //Run GetForeGroundWindows and get active window informations
+            //assign them into handle pointer variable
+            IntPtr handle = IntPtr.Zero;
+            handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, ss, nChar) > 0) return ss.ToString();
+            else return "";
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (status.Text == "Running")
+            {
+                status.Text = "Stopped";
+                button.Text = "Start";
+                tmr.Stop();
+                tmr2.Stop();
+            }
+            else
+            {
+                status.Text = "Running";
+                button.Text = "Stop";
+                tmr.Start();
+                tmr2.Start();
+            }
+        }
+    }
+}
